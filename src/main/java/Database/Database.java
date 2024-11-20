55b184c2ec7b84c339192ec1284e8eec4a53d606
thhang05 <thuyhangk40bhxh@gmail.com>
2024-11-20 17:44:01 +0700

    config.properties update

---
+++ src/main/java/Database/Database.java
@@ -0,0 +1,12 @@
+package database;
+
+import java.io.InputStream;
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Properties;
+
+public class Database {
+
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module org.example.btloop {
     requires javafx.fxml;
     requires javafx.web;
 
+    requires java.sql;
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
